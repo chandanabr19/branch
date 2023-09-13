93eeec272706d21d8074a89d30ac82cb1f5adb04
chandanabr19 <brchandana19@gmail.com>
2023-09-13 20:31:44 +0530

    grapes

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+x={1:'computer','name':'for',3:'science'}
+print("accessing a element using key:")
+print(x['name'])
+print("dictionary 1:",x[3])
+print("element at indexing 1",x['name'])
+x1={234:'A',456:'B'}
+x2={678:'C',890:'D'}
+x3={234:'A',456:'B'}
+a=x1==x2
+b=x1==x3
+print('x1 is equal to x2:','a')
+print('x1 is equal to x3:','b')
+print('234 in x1:',234 in x1)
+print('456 in x2:',456 in x2)
+
+
+
